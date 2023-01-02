06807d012e3792a3288a242d71eaeedfdd020b79
ChazG <chaz.mykl@gmail.com>
2023-01-02 11:36:23 -0700

    Initial Commit

---
+++ NiteLifeApp/NiteLifeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiteLifeAppApp.swift
+//  NiteLifeApp
+//
+//  Created by Chaz Mykl on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct NiteLifeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
